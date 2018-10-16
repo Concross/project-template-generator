2ef4535c675b2242d0dfdaf098708143b1921013
Concross <cacrossley22@gmail.com>
2018-10-16 14:42:33 -0700

    update react template

---
+++ templates/react-project/src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './footer.scss';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <footer id="page-footer">
+        <p>I am a footer</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
